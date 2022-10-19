2a608327d5ae6b69dc00b33913897eb2078d3c74
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-10-19 15:47:08 +0300

    Add files via upload

---
+++ lab9/ff.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+int main(){
+int i0, j0, l0, i, j, l, f, g;
+ i0 = 6;
+ j0 = 27;
+ l0 = -15;
+
+for (int k = 0; k <= 50; k++){
+
+i = (i0*i0*i0 - j0*j0*j0 + l0*l0*l0 - k)%20;
+j = min(i0*j0*l0 - k, min(i0*i0*l0 - k, j0*l0*l0 - k))%30;
+l = max(i0*j0*l0 - k, max(i0*i0*l0 - k, j0*l0*l0 - k))%30;
+l0 = l;
+i0 = i;
+j0 = j;
+f = pow((i-(-10)),2) + pow((j - (-10)),2);
+g = pow((i-(-20)),2) + pow((j - (-20)),2);
+
+printf("k=%d i=%d j=%d l=%d 100<%d 100<%d\n", k, i, j, l, f, g);
+}
+
+return 0;
+}
