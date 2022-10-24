ac201aaef9a8f25053190a34c3326c0df449b11d
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-10-24 22:06:35 +0300

    Add files via upload

---
+++ lab8/cc.cpp
@@ -0,0 +1,26 @@
+﻿#include <assert.h>
+#include <stdio.h>
+int ipow(int base, int exp);
+
+int main(void) {
+	int base, exp;
+	scanf_s("%d%d",&base,&exp);
+	int result = ipow(base, exp);
+	printf("%d", result);
+	return 0;
+}
+
+int ipow(int base, int exp) {
+	int result = 1;
+	for (; exp != 0; ) {
+		if (exp % 2 == 0) {
+			exp = exp / 2;
+            base *= base;	
+		}
+		else {
+			--exp;
+			result *= base;
+		}		
+	}
+	return result;
+}
