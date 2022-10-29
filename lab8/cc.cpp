5775ce482f1bedc0323fe74552a63883c28058dc
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-10-29 12:46:03 +0300

    Update cc.cpp

--- lab8/cc.cpp
+++ lab8/cc.cpp
@@ -1,10 +1,10 @@
-﻿#include <assert.h>
+#include <assert.h>
 #include <stdio.h>
 int ipow(int base, int exp);
 
 int main(void) {
 	int base, exp;
-	scanf_s("%d%d",&base,&exp);
+	scanf_s("%d%d", &base, &exp);
 	int result = ipow(base, exp);
 	printf("%d", result);
 	return 0;
@@ -15,12 +15,12 @@ int ipow(int base, int exp) {
 	for (; exp != 0; ) {
 		if (exp % 2 == 0) {
 			exp = exp / 2;
-            base *= base;	
+			base *= base;
 		}
 		else {
 			--exp;
 			result *= base;
-		}		
+		}
 	}
 	return result;
 }
