8f85487a491f1dae446ff451958955b6a161f94c
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-10-29 12:45:19 +0300

    Update gg.c

--- lab8/gg.c
+++ lab8/gg.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-int main(){
-   long double a, b, c;
-   scanf ("%LF%LF", &a, &b);
-   c = a + b;
-   printf ("c=%LF\n", c);
-return 0;
+#include<stdio.h>
+
+int main() {
+
+	long double a, b, c;
+
+	scanf("%LF%LF", &a, &b);
+
+	c = a + b;
+
+	printf("c=%LF\n", c);
+
+	return 0;
+
 }
