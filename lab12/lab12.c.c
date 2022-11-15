8250947c4a5195b9c2446ad8fcf699d2be84fc3b
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-11-15 22:12:47 +0300

    Add files via upload

---
+++ lab12/lab12.c.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main() {
+    long N;
+    scanf_s("%ld", &N);
+    int d, k;
+    int c = 0;
+    k = N % 10;
+    while (N != 0) {
+        N = N / 10;
+        d = N % 10;
+        if (d == k) {
+            c = 1;
+            break;
+        }
+        k = d;
+    }
+    if (c) printf("NO\n");
+    else printf("DIFFERENT\n");
+    return 0;
+}
