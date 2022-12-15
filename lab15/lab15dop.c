ba5c88af6fad02f3cc3d04af7fca2ed8671b6ad4
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2022-12-16 02:40:08 +0300

    Add files via upload

---
+++ lab15/lab15dop.c
@@ -0,0 +1,39 @@
+/* conc(a, b) = a * 10^(len(b)) + b
+   a * b + a + b = a * 10^(len(b)) + b
+   b = 10^(len(b)) - 1, т.е. при b = 9,99,...,99...9 равенство верное
+   т.о. ответом будет кол-во чисел типа b в диапозоне от 1 до B, умноженное на a
+*/
+#include <stdio.h>
+        
+int length(int num) {
+    int rslt_num = 0;
+    int len = 0;
+    while(num != 0) {
+        rslt_num += num % 10;
+        num /= 10;
+        ++len;
+    }
+    if(rslt_num / 9 == len) {
+        return len;
+    } else {
+        return len - 1;
+    }
+}
+
+void counting_pairs() {
+    int t;
+    scanf("%d\n", &t);
+    for(int i = 0; i < t; ++i){
+        int a,b;
+        int num_pairs = 0;
+        scanf("%d %d", &a, &b);
+        num_pairs = a * length(b);
+        printf("%d\n", num_pairs);
+    }
+}
+
+int main() {
+    counting_pairs();
+    return 0;
+}
+     
