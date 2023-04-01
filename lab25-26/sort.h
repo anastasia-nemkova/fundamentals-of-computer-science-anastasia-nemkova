36d09e03adecc3026204cad4fbd31549510ea3c9
anastasia-nemkova <113765837+anastasia-nemkova@users.noreply.github.com>
2023-04-01 05:37:24 +0300

    Add files via upload

---
+++ lab25-26/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include "dec.h"
+
+void DeckArr(deck *D, int N, int *mas);
+void QuickSort(int* s_arr, int first, int last);
+void ArrDeck(deck *D, int N, int *mas);
+
+#endif // SORT_H
